269f6b54812fc0c25e757914e5e8b15661fa78d0
jasmine <jasmine@stp.dias.ie>
2025-11-04 16:17:57 +0000

    Remove bloop from imports

--- Source/UnitTests.py
+++ Source/UnitTests.py
@@ -1,10 +1,10 @@
 if __name__ == "__main__":
     # These imports are used despite what ruff thinks
-    from Bloop.PythoniseMathematica import PythoniseMathematicaUnitTests # noqa: F401
-    from Bloop.ParsedExpression import ParsedExpressionUnitTests # noqa: F401
-    from Bloop.TrackVEV import TrackVEVUnitTests # noqa: F401
-    from Bloop.Z2_ThreeHiggsBmGenerator import BmGeneratorUnitTests # noqa: F401
-    from Bloop.PDGData import PDGUnitTests # noqa: F401
+    from PythoniseMathematica import PythoniseMathematicaUnitTests # noqa: F401
+    from ParsedExpression import ParsedExpressionUnitTests # noqa: F401
+    from TrackVEV import TrackVEVUnitTests # noqa: F401
+    from Z2_ThreeHiggsBmGenerator import BmGeneratorUnitTests # noqa: F401
+    from PDGData import PDGUnitTests # noqa: F401
 
     from unittest import main
 
