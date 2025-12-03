129a918c2cea00b009d24da066e463ecc53b8a3b
Jasmine <jasmine@stp.dias.ie>
2025-12-03 15:21:33 +0000

    Use if main() function and if name == name (needed for pip)

--- Source/RunStages.py
+++ Source/RunStages.py
@@ -1,25 +1,28 @@
-from importlib import import_module
-from UserInput import UserInput, Stages
+def main():
+    from importlib import import_module
+    from UserInput import UserInput, Stages
+    args = UserInput().parse()
 
-args = UserInput().parse()
+    if args.firstStage <= Stages.convertMathematica <= args.lastStage:
+        if args.verbose:
+            print("Convert Mathematica stage started")
 
-if args.firstStage <= Stages.convertMathematica <= args.lastStage:
-    if args.verbose:
-        print("Convert Mathematica stage started")
+        from PythoniseMathematica import pythoniseMathematica
 
-    from PythoniseMathematica import pythoniseMathematica
+        pythoniseMathematica(args)
 
-    pythoniseMathematica(args)
+    if args.firstStage <= Stages.generateBenchmark <= args.lastStage:
+        if args.verbose:
+            print("Benchmark generation stage started")
+        import_module(args.bmGeneratorModule).generateBenchmarks(args)
 
-if args.firstStage <= Stages.generateBenchmark <= args.lastStage:
-    if args.verbose:
-        print("Benchmark generation stage started")
-    import_module(args.bmGeneratorModule).generateBenchmarks(args)
+    if args.firstStage <= Stages.doMinimization <= args.lastStage:
+        if args.verbose:
+            print("Minimization stage started")
 
-if args.firstStage <= Stages.doMinimization <= args.lastStage:
-    if args.verbose:
-        print("Minimization stage started")
+        from LoopBenchmarks import loopBenchmarks
 
-    from LoopBenchmarks import loopBenchmarks
+        loopBenchmarks(args)
 
-    loopBenchmarks(args)
+if __name__ == "__main__":
+        main()
