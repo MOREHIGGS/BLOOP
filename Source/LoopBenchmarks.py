b4bf05d4cf2b863c0c258a0423c49f4aa4762831
Jasmine Thomson-Cooke <jasmine@stp.dias.ie>
2025-12-12 16:46:10 +0000

    Move processMin inside LoopBenchmarks

--- Source/LoopBenchmarks.py
+++ Source/LoopBenchmarks.py
@@ -6,11 +6,56 @@ from ijson import items
 from importlib import import_module
 from functools import partial
 from tqdm import tqdm
+import numpy as np
 
 from TrackVEV import TrackVEV, cNlopt
-from ProcessMinimization import interpretData
 from PythoniseMathematica import replaceGreekSymbols
 from ParsedExpression import ParsedExpressionSystemArray
+
+
+def interpretData(result, bmNumber, bmInput, fieldNames):
+    processedResult = {
+        "bmNumber": bmNumber,
+        "bmInput": bmInput,
+        "failureReason": result["failureReason"],
+        "fieldJumps": None,
+        "strong": False,
+        "steps": 0
+    }
+
+    if result["failureReason"]:
+        return processedResult 
+
+    processedResult |= {"isPerturbative": bool(np.all(result["bIsPerturbative"])),
+                        "complex": bool(np.any(
+                                 np.abs( np.array(result["vevDepthImag"]) / np.array(result["vevDepthReal"])
+                                        ) > 1e-8
+                                        ))
+                        }
+      
+
+    allFieldValues = result["vevLocation"] / np.sqrt(result["T"])
+    allFieldValuesD = np.diff(allFieldValues)
+    allFieldValuesT = allFieldValues.transpose() 
+    
+    fieldLengthDiff = np.array([ np.linalg.norm(allFieldValuesT[idx]) 
+                       - np.linalg.norm(allFieldValuesT[idx+1]) 
+                       for idx in range(len(allFieldValuesT)-1) ])  
+    PTIndices = (fieldLengthDiff > 0.6).nonzero()[0]
+    if len(PTIndices) > 0:
+        processedResult["steps"] = len(fieldLengthDiff[PTIndices])
+        processedResult["strong"] = float(max(fieldLengthDiff[PTIndices]))
+        results = []
+        
+        for idx in PTIndices:
+            resultDic = {}
+            for fieldNameIdx, fieldJumps in enumerate(allFieldValuesD):
+                if abs(fieldJumps[idx]) > 0.1:
+                    resultDic[fieldNames[fieldNameIdx]] = float(fieldJumps[idx])
+            results.append(resultDic)
+        
+        processedResult["fieldJumps"] = results
+    return processedResult
 
 
 ## This (sometimes) avoids floating point error in T gotten by np.arange or linspace
