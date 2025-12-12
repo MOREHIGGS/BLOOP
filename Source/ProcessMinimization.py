41f92b155f9b104de516babb5bb48474545bd0dd
Jasmine Thomson-Cooke <jasmine@stp.dias.ie>
2025-12-12 15:40:03 +0000

    Finished strength + factorisation

--- Source/ProcessMinimization.py
+++ Source/ProcessMinimization.py
@@ -1,67 +1,47 @@
 import numpy as np
 
-def PTStrength(idx, fields):
-    store = np.zeros(2)
-    for i in range(2):
-        for field in fields:
-            store[i] += field[idx + i] ** 2
-    return np.sqrt(store[0] - store[1])
-
 def interpretData(result, bmNumber, bmInput, fieldNames):
     processedResult = {
         "bmNumber": bmNumber,
         "bmInput": bmInput,
+        "failureReason": result["failureReason"],
+        "fieldJumps": None,
         "strong": False,
-        "complex": False,
-        "results": None,
+        "steps": 0
     }
 
     if result["failureReason"]:
-        return processedResult | {"failureReason": result["failureReason"]}
+        return processedResult 
 
-    PTTemps = set()
+    processedResult |= {"isPerturbative": bool(np.all(result["bIsPerturbative"])),
+                        "complex": bool(np.any(
+                                 np.abs( np.array(result["vevDepthImag"]) / np.array(result["vevDepthReal"])
+                                        ) > 1e-8
+                                        ))
+                        }
+      
+
     allFieldValues = result["vevLocation"] / np.sqrt(result["T"])
+    allFieldValuesD = np.diff(allFieldValues)
     allFieldValuesT = allFieldValues.transpose() 
+    
     fieldLengthDiff = np.array([ np.linalg.norm(allFieldValuesT[idx]) 
                        - np.linalg.norm(allFieldValuesT[idx+1]) 
                        for idx in range(len(allFieldValuesT)-1) ])  
     
     PTIndices = (fieldLengthDiff > 0.6).nonzero()
     if len(PTIndices) > 0:
-        processedResult["strong"] = max(fieldLengthDiff[PTIndices])
+        processedResult["steps"] = len(fieldLengthDiff[PTIndices])
+        processedResult["strong"] = float(max(fieldLengthDiff[PTIndices]))
+        results = []
+        
         for idx in PTIndices:
-            pass            
-            
+            resultDic = {}
+            for fieldNameIdx, fieldJumps in enumerate(allFieldValuesD):
+                if abs(fieldJumps[idx]) > 0.1:
+                    resultDic[fieldNames[fieldNameIdx]] = float(fieldJumps[idx])
+            results.append(resultDic)
+        
+        processedResult["fieldJumps"] = results
 
-
-    for idx, fieldValues in enumerate(allFieldValues):
-        ## Find the indices where a field (dimentionless) changes by more than 0.3
-        PTindices = np.nonzero(np.abs(np.diff(fieldValues)) > 0.3)[0]
-
-        if len(PTindices) > 0:
-            strengthResults = []
-            for PTindex in PTindices:
-                strength = float(PTStrength(PTindex, allFieldValues))
-                T = float(result["T"][PTindex])
-
-                strengthResults.append([strength, T])
-                PTTemps.add(T)
-
-                if not processedResult["strong"]:
-                    processedResult["strong"] = strength if strength > 0.6 else False
-                if processedResult["strong"]:
-                    processedResult["strong"] = (
-                        strength
-                        if strength > processedResult["strong"]
-                        else processedResult["strong"]
-                    )
-
-            processedResult["results"][f"{fieldNames[idx]}"] = strengthResults
-
-    processedResult["steps"] = len(PTTemps)
-    processedResult["bIsPerturbative"] = bool(np.all(result["bIsPerturbative"]))
-    imag2RealRatio = abs(
-        np.array(result["vevDepthImag"]) / np.array(result["vevDepthReal"])
-    )
-    processedResult["complex"] = bool(np.any(imag2RealRatio > 1e-8))
     return processedResult
