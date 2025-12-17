833145ecac4061f37b118bd9062fa1b86a16792b
Jasmine Thomson-Cooke <jasmine@stp.dias.ie>
2025-12-17 14:45:23 +0000

    Works for generic model now

--- Source/PostProcessing/ScatterCompareNEW.py
+++ Source/PostProcessing/ScatterCompareNEW.py
@@ -1,27 +1,26 @@
-from glob import glob  ## I think glob lets you do the * thingy
 import json
 from matplotlib import pylab as plt
-from numpy import transpose
+import numpy as np
 from pathlib import Path
 
-def loadData(scanResultsList):
+def getData(scanResultsList):
     strengthList = []
     bmInputList = []
     TcList = []
     bmNumberList = []
 
     for scanResults in scanResultsList:
-        with open(directory, "r") as fp:
+        with open(scanResults, "r") as fp:
             data = json.load(fp)
-
+        
         if len(data) == 0:
-            print(f"{directory} is empty")
+            print(f"{scanResults} is empty")
             next()
 
-        for result in scanResults:
-            if data["strong"] > 0.6:
-                strength.append((result["strong"]))
-                bmInput.append((list(result["bmInput"].values())))
+        for result in data:
+            if result["strong"] > 0.6:
+                strengthList.append((result["strong"]))
+                bmInputList.append((list(result["bmInput"].values())))
                 
                 Tc = 0
 
@@ -30,154 +29,62 @@ def loadData(scanResultsList):
                     Tc = jumpResults["Tc"] if jumpResults["Tc"] > Tc else Tc
 
                 TcList.append(Tc)
-                bmNumber.append(resultDic["bmNumber"])
+                bmNumberList.append(result["bmNumber"])
+    ## Sort bmInputs by order of strength, 
+    ## this is so the colour of the scatter plot is set by the strong PT at that point
+    ## transpose taken so each row is just on variable type (faster to plot)
+    return np.transpose(sorted(zip(strengthList, bmNumberList, TcList, *np.transpose(bmInputList)))), list(result["bmInput"].keys())
 
-    return strengthList, bmInputList, TcList, bmNumberList
 
-# do something like filesToLoad = [dir/Result_{i} for i in range(n)]
-strength2Loop, bmInput2Loop, Tc2Loop, bmNumber2Loop = loadData(
-    ["2LoopResults/Combined01SSS"]
-)
-strength1Loop, bmInput1Loop, Tc1Loop, bmNumber1Loop = loadData(
-    ["1LoopResults/Combined01SSS"]
-)
+data1Loop, axisLabels = getData(["results1.json", "results2.json"])
+data2Loop, _ = getData(["2results1.json", "2results2.json"])
+dataTotal = (data1Loop, data2Loop)
 
-# Sort the bmInputs by order of strength, this is so the colour of the scatter plot is set by the strong PT at that point, transpose taken so each row is just on variable type
-(
-    strength2Loop,
-    theta2Loop,
-    gHDM12Loop,
-    ms12Loop,
-    delta122Loop,
-    delta1c2Loop,
-    deltac2Loop,
-    _,
-    Tc2Loop,
-    bmNumber2Loop,
-) = zip(*sorted(zip(strength2Loop, *transpose(bmInput2Loop), Tc2Loop, bmNumber2Loop)))
+norm = plt.Normalize(0.6, max(max(dataTotal[0][0]), max(dataTotal[1][0])))
 
-(
-    strength1Loop,
-    theta1Loop,
-    gHDM11Loop,
-    ms11Loop,
-    delta121Loop,
-    delta1c1Loop,
-    deltac1Loop,
-    _,
-    Tc1Loop,
-    bmNumber1Loop,
-) = zip(*sorted(zip(strength1Loop, *transpose(bmInput1Loop), Tc1Loop, bmNumber1Loop)))
+fileNames = axisLabels
 
-strongest = max(strength1Loop + strength2Loop)
-weakest = 0.6
-norm = plt.Normalize(weakest, strongest)
-
-# This is magic that makes plots, might rewrite later
-inputData = (
-    (
-        Tc2Loop,
-        ms12Loop,
-        theta2Loop,
-        gHDM12Loop,
-        delta122Loop,
-        delta1c2Loop,
-        deltac2Loop,
-        strength2Loop,
-    ),
-    (
-        Tc1Loop,
-        ms11Loop,
-        theta1Loop,
-        gHDM11Loop,
-        delta121Loop,
-        delta1c1Loop,
-        deltac1Loop,
-        strength1Loop,
-    ),
-)
-
-ylabels = [
-    "$m_{s1}$ (GeV)",
+## ~~~~ For nicer axis labels~~~~
+axisLabels = [
     "$\\theta_{\\text{CPV}}$",
     "$g_{\\text{hDM}}$",
+    "$m_{s1}$ (GeV)",
     "$\\delta_{12} \\  (GeV)$",
     "$\\delta_{1c} \\  (GeV)$",
     "$\\delta_{c} \\  (GeV)$",
+    "n"
 ]
-fileNames = ["ms101", "Theta01", "GHDM01", "Delta1201", "Delta1c01", "Deltac01"]
-loopList = ["2Loop", "1Loop"]
-for idxL, dataRow in enumerate(inputData):
-    # Makes plots of ms1 vs other stuff
-    for idxI, data in enumerate(dataRow[2:-1]):
-        # +1 to skip over Tc
-        idxI += 1
-        plt.scatter(dataRow[1], data, s=4.2**2, c=dataRow[-1], marker="o", norm=norm)
-        plt.xlabel("$m_{s1}$ (GeV)", labelpad=5, fontsize=12)
-        plt.ylabel(ylabels[idxI], labelpad=5, fontsize=12)
+## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+loopList = ["1Loop", "2Loop"]
+
+for loopIdx, dataLoop in enumerate(dataTotal):
+    fileDir = f"ScanFigures/{loopList[loopIdx]}"
+    Path(fileDir).mkdir(parents=True, exist_ok=True)
+    
+    # Makes plots of first bm Input vs rest of bm inputs
+    for inputIdx, data in enumerate(dataLoop[4:]):
+        plt.scatter(dataLoop[3], data, s=4.2**2, c=dataLoop[0], marker="o", norm=norm)
+        plt.xlabel(axisLabels[0], labelpad=5, fontsize=12)
+        ## +1 needed to skip zeroth element
+        print(inputIdx, axisLabels[inputIdx])
+        plt.ylabel(axisLabels[inputIdx+1], labelpad=5, fontsize=12)
         plt.colorbar(
             ticks=(0.60, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95, 1, 1.05, 1.10, 1.15),
             label="strength",
         )
-        fileDir = f"TestFigures/{loopList[idxL]}"
-        Path(fileDir).mkdir(parents=True, exist_ok=True)
-        plt.savefig(fileDir + f"/{fileNames[idxI]}")
+        plt.savefig(fileDir + f"/{fileNames[inputIdx+1]}")
         plt.close()
-    # Makes plots of Tc vs other stuff
-    for idxI, data in enumerate(dataRow[1:-1]):
-        plt.scatter(data, dataRow[0], s=4.2**2, c=dataRow[-1], marker="o", norm=norm)
-        plt.xlabel(ylabels[idxI], labelpad=5, fontsize=12)
+        
+    # Makes plots of bm inputs vs Tc
+    for inputIdx, data in enumerate(dataLoop[3:]):
+        
+        plt.scatter(data, dataLoop[2], s=4.2**2, c=dataLoop[0], marker="o", norm=norm)
+        plt.xlabel(axisLabels[inputIdx], labelpad=5, fontsize=12)
         plt.ylabel("$T_c$ (GeV)", labelpad=5, fontsize=12)
         plt.colorbar(
             ticks=(0.60, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95, 1, 1.05, 1.10, 1.15),
             label="strength",
         )
-        fileDir = f"TestFigures/{loopList[idxL]}"
-        Path(fileDir).mkdir(parents=True, exist_ok=True)
-        plt.savefig(fileDir + f"/Tc{fileNames[idxI]}")
+        plt.savefig(fileDir + f"/Tc{fileNames[inputIdx]}")
         plt.close()
-
-exit()
-
-items = 50
-plt.scatter(
-    ms12Loop[-items:],
-    theta2Loop[-items:],
-    s=4.6**2,
-    c=strength2Loop[-items:],
-    marker="o",
-    norm=norm,
-)
-for bm in bmNumber2Loop[-items:]:
-    index = bmNumber1Loop.index(bm)
-    plt.scatter(
-        ms11Loop[index] * 1.005,
-        theta1Loop[index],
-        s=4.6**2,
-        c=strength1Loop[index],
-        marker="*",
-        norm=norm,
-    )
-plt.xlabel("$m_{s1}$ (GeV)", labelpad=5, fontsize=12)
-plt.ylabel("$\\theta_{\\text{CPV}}$ ", labelpad=5, fontsize=12)
-plt.colorbar(
-    ticks=(0.60, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95, 1, 1.05, 1.10, 1.15),
-    label="strength",
-)
-plt.savefig("ScanFigures/TEST")
-plt.close()
-
-##Puts 1 and 2 loop into one figure
-# f, axes = plt.subplots(nrows = 1, ncols = 2)
-# norm=plt.Normalize(weakest,strongest)
-# sc = axes[0].scatter(ms11Loop, theta1Loop,s=4.2**2, c = strength1Loop, marker = "o", norm=norm)
-# axes[1].scatter(ms12Loop, theta2Loop,s=4.2**2, c = strength2Loop, marker = "o", norm=norm)
-# for i in range(len(axes)):
-#    axes[i].set_xlabel("$m_{s1}$ (GeV)", labelpad = 5)
-#    axes[i].set_ylabel("$\\theta_{\\text{cpv}}$ ", labelpad = 5)
-# f.set_figwidth(15)
-# cbar_ax = f.add_axes([.92, .15, 0.02, 0.7])
-#
-# f.colorbar(sc, cax=cbar_ax, ticks = (0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95, 1, 1.05), label = "strength")
-#
-# plt.show()
