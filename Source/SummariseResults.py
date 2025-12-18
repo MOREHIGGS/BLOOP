4dc70bcc21f7fb84de3354154f891eb4a4728fbb
Jasmine Thomson-Cooke <jasmine@stp.dias.ie>
2025-12-18 13:28:27 +0000

    summarise safe if no data, summary saved in file (wip) rather than printed

--- Source/SummariseResults.py
+++ Source/SummariseResults.py
@@ -1,4 +1,5 @@
 from json import load
+from textwrap import dedent
 from collections import defaultdict
 from matplotlib import pylab as plt
 import numpy as np
@@ -54,50 +55,53 @@ def summariseResults(args):
     ## this is so the colour of the scatter plot is set by the strong PT at that point
     ## transpose taken so each row is just on variable type (faster to plot)
     dataSorted =  np.transpose(np.asarray(sorted(zip(strengthList, bmNumberList, TcList, *np.transpose(bmInputList))))) 
-    axisLabels = list(result["bmInput"].keys())
-    print("Summary of the results:")
-    print(f"The lowest Tc min is: {min(dataSorted[2])}")
-    print(f"The strongest BM is: {dataSorted[0][-1]} (strength), {dataSorted[1][-1]} (bmNumber)")
-    print(f"The number of benchmarks is: {len(data)}")
-    print(f"The number of strong benchmarks is: {len(dataSorted[0])}")
-    print(f"The number of mutli step phase transitions is: {multiStepCount}")
-    print(f"The number of failed benchmarks is: {failDict.items()}")
-    print(f"The number of benchmarks with a complex min is: {complexCount}")
-    print(f"The number of benchmarks that become non-pert is: {nonPertCount}")
+    if len(dataSorted[0]) > 0:
+        with open("outputSummary.txt", "w") as fp:
+            fp.writelines(dedent(f"""\
+                Summary of the results: 
+                The lowest Tc min is: {min(dataSorted[2])} 
+                The strongest BM is: {dataSorted[0][-1]} (strength), {dataSorted[1][-1]} (bmNumber) 
+                The number of benchmarks is: {len(data)} 
+                The number of strong benchmarks is: {len(dataSorted[0])} 
+                The number of mutli step phase transitions is: {multiStepCount} 
+                The number of failed benchmarks is: {failDict.items()} 
+                The number of benchmarks with a complex min is: {complexCount} 
+                The number of benchmarks that become non-pert is: {nonPertCount}
+                """))
+        # Is this still needed?
+        norm = plt.Normalize(dataSorted[0][0], dataSorted[0][-1])
+        
+        axisLabels = list(result["bmInput"].keys())
+        fileNames = axisLabels
+
+        ## ~~~~ For nicer axis labels~~~~
+        #axisLabels = [
+        #    "$\\theta_{\\text{CPV}}$",
+        #    "$g_{\\text{hDM}}$",
+        #    "$m_{s1}$ (GeV)",
+        #    "$\\delta_{12} \\  (GeV)$",
+        #    "$\\delta_{1c} \\  (GeV)$",
+        #    "$\\delta_{c} \\  (GeV)$",
+        #    "n"
+        #]
+        ## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+        # Makes plots of first bm Input vs rest of bm inputs
+        for inputIdx, data in enumerate(dataSorted[4:]):
+            plt.scatter(dataSorted[3], data, s=4.2**2, c=dataSorted[0], marker="o", norm=norm)
+            plt.xlabel(axisLabels[0], labelpad=5, fontsize=12)
+            ## +1 needed to skip zeroth element
+            plt.ylabel(axisLabels[inputIdx+1], labelpad=5, fontsize=12)
+            plt.colorbar(label="strength")
+            plt.savefig(f"{args.resultsDirectory}/ScanFigures/{fileNames[inputIdx+1]}")
+            plt.close()
     
-    # Is this still needed?
-    norm = plt.Normalize(dataSorted[0][0], dataSorted[0][-1])
-
-    fileNames = axisLabels
-
-    ## ~~~~ For nicer axis labels~~~~
-    #axisLabels = [
-    #    "$\\theta_{\\text{CPV}}$",
-    #    "$g_{\\text{hDM}}$",
-    #    "$m_{s1}$ (GeV)",
-    #    "$\\delta_{12} \\  (GeV)$",
-    #    "$\\delta_{1c} \\  (GeV)$",
-    #    "$\\delta_{c} \\  (GeV)$",
-    #    "n"
-    #]
-    ## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-    # Makes plots of first bm Input vs rest of bm inputs
-    for inputIdx, data in enumerate(dataSorted[4:]):
-        plt.scatter(dataSorted[3], data, s=4.2**2, c=dataSorted[0], marker="o", norm=norm)
-        plt.xlabel(axisLabels[0], labelpad=5, fontsize=12)
-        ## +1 needed to skip zeroth element
-        plt.ylabel(axisLabels[inputIdx+1], labelpad=5, fontsize=12)
-        plt.colorbar(label="strength")
-        plt.savefig(f"{args.resultsDirectory}/ScanFigures/{fileNames[inputIdx+1]}")
-        plt.close()
-    
-    # Makes plots of bm inputs vs Tc
-    for inputIdx, data in enumerate(dataSorted[3:]):
-        plt.scatter(data, dataSorted[2], s=4.2**2, c=dataSorted[0], marker="o", norm=norm)
-        plt.xlabel(axisLabels[inputIdx], labelpad=5, fontsize=12)
-        plt.ylabel("$T_c$ (GeV)", labelpad=5, fontsize=12)
-        plt.colorbar(label="strength")
-        plt.savefig(f"{args.resultsDirectory}/ScanFigures/Tc{fileNames[inputIdx]}")
-        plt.close()
+        # Makes plots of bm inputs vs Tc
+        for inputIdx, data in enumerate(dataSorted[3:]):
+            plt.scatter(data, dataSorted[2], s=4.2**2, c=dataSorted[0], marker="o", norm=norm)
+            plt.xlabel(axisLabels[inputIdx], labelpad=5, fontsize=12)
+            plt.ylabel("$T_c$ (GeV)", labelpad=5, fontsize=12)
+            plt.colorbar(label="strength")
+            plt.savefig(f"{args.resultsDirectory}/ScanFigures/Tc{fileNames[inputIdx]}")
+            plt.close()
 
