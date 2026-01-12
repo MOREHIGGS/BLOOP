2651bba70cdaaa47e6814d71eea931ec66c48c34
Jasmine Thomson-Cooke <jasmine@stp.dias.ie>
2026-01-12 14:05:20 +0000

    WIP Rename variable and set omp_num_threads to 1 by default

--- Share/RunStagesWrapper.sh
+++ Share/RunStagesWrapper.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 # PYTHON_ARGCOMPLETE_OK
 
-if [ -n "$BLOOP_SHARE_DIR" ]; then
-    echo container
-    BASE_DIR="$BLOOP_SHARE_DIR"
+if [ -n "$SHARE_DIR" ]; then
+    ## This is an env variable set in the docker to say we are in container
+    ## as this script is copied to a different location so below doesn't work
+    SHARE_DIR="$SHARE_DIR"
 else
-    echo non contianer
-    BASE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+    SHARE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 fi
 
-python3 $BASE_DIR/../Source/RunStages.py "$@"
+## Sets OMP_THREADS to 1 unless user sets it manually with env variabable
+## "$@" needed for autocomplete on cmd line args
+OMP_NUM_THREADS="${OMP_NUM_THREADS:-1}" python3 $SHARE_DIR/../Source/RunStages.py "$@"
