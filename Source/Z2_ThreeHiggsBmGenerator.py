1f8a5491733c4db47385d0ccc87e391be481db3d
Jasmine Thomson-Cooke <jasmine@stp.dias.ie>
2026-01-26 15:45:05 +0000

    Reordered functions

--- Source/Z2_ThreeHiggsBmGenerator.py
+++ Source/Z2_ThreeHiggsBmGenerator.py
@@ -1,6 +1,4 @@
-"""Generate benchmarks at 0T satisfying some constraints.
-If the functions return True then the benchmark point is allowed by those constraints
-Variables that are bools are denoted with a b prefix"""
+"""Generate benchmarks at 0T satisfying some constraints."""
 
 import math as m
 import numpy as np
@@ -15,74 +13,159 @@ from ParsedExpression import ParsedExpression
 from TrackVEV import cNlopt
 from PDGData import mHiggs, higgsVEV, mTop, mW, mZ
 
+def generateBenchmarks(args):
+    (output_file := Path(args.benchmarkFilePath)).parent.mkdir(exist_ok=True, parents=True)
 
-def bIsBounded(params):
-    ## Taking equations 26-31 from the draft that ensure the potential is bounded from below.
-    if not params["lamda11"] > 0:
-        return False
-    if not params["lamda22"] > 0:
-        return False
-    if not params["lamda33"] > 0:
-        return False
-
-    lamdax = params["lamda12"] + min(
-        0, params["lamda12p"] - 2 * m.sqrt(params["lamda1Re"] ** 2 + params["lamda1Im"] ** 2)
+    ## Need to take these from args
+    nloptInst = cNlopt(
+        config={
+            "nbrVars": 3,
+            "absGlobalTol": 0.5,
+            "relGlobalTol": 0.5,
+            "absLocalTol": 0.5,
+            "relLocalTol": 0.5,
+            "varLowerBounds": [-300, 0, 0],
+            "varUpperBounds": [300, 300, 300],
+        }
     )
-    lamday = params["lamda31"] + min(
-        0, params["lamda31p"] - 2 * m.sqrt(params["lamda3Re"] ** 2 + params["lamda3Im"] ** 2)
+    with open(args.pythonisedExpressionsFilePath, "r") as fp:
+        parsedExpressions = json.load(fp)
+    ## Take the pythonised tree level potential we've generated
+    treeLevel = ParsedExpression(parsedExpressions["veff"]["expressions"], None)
+    chargedMassMatrix = ParsedExpression(
+        parsedExpressions["scalarMassMatrices"]["expressions"][0], None
     )
-    lamdaz = params["lamda23"] + min(
-        0, params["lamda23p"] - 2 * m.sqrt(params["lamda2Re"] ** 2 + params["lamda2Im"] ** 2)
+    neutralMassMatrix = ParsedExpression(
+        parsedExpressions["scalarMassMatrices"]["expressions"][1], None
     )
-    if not lamdax > -2 * m.sqrt(params["lamda11"] * params["lamda22"]):
-        return False
-    if not lamday > -2 * m.sqrt(params["lamda11"] * params["lamda33"]):
-        return False
-    if not lamdaz > -2 * m.sqrt(params["lamda22"] * params["lamda33"]):
-        return False
-    if not (
-        m.sqrt(params["lamda33"]) * lamdax
-        + m.sqrt(params["lamda11"]) * lamdaz
-        + m.sqrt(params["lamda22"]) * lamday
-        >= 0
-        or params["lamda33"] * lamdax**2
-        + params["lamda11"] * lamdaz**2
-        + params["lamda22"] * lamday**2
-        - params["lamda11"] * params["lamda22"] * params["lamda33"]
-        - 2 * lamdax * lamday * lamdaz
-        < 0
-    ):
-        return False
-    return True
 
+    ## Feels weird to have nested function but not sure how else to go until can
+    ## use arrays with nlopt
+    def potential(fields, params):
+        params["v1"] = fields[0]
+        params["v2"] = fields[1]
+        params["v3"] = fields[2]
+        return treeLevel.evaluate(params)
 
-def bPhysicalMinimum(nloptInst, potential, params):
-    ## Move these to user args or somewhere out of the way
-    minimumInitialGuesses = [
-        [0, 0, 0],
-        [0, 0, 246],
-        [100, 100, 100],
-        [-100, 100, 100],
-        [50, 50, 50],
-        [299, 299, 299],
-        [-299, 299, 299],
+    if args.benchmarkType == "randomSSS":
+        with open(output_file, "w") as fp:
+            json.dump(_strongSubSet(args.prevResultDir), fp, indent=4)
+        return
+
+    elif args.benchmarkType == "handPicked":
+        with open(output_file, "w") as fp:
+            json.dump(
+                _handPickedBm(nloptInst, potential, chargedMassMatrix, neutralMassMatrix),
+                fp,
+                indent=4,
+            )
+
+    elif args.benchmarkType == "random":
+        with open(output_file, "w") as fp: 
+            json.dump(
+                _randomBmParam(
+                    args.randomNum,
+                    nloptInst,
+                    potential,
+                    chargedMassMatrix,
+                    neutralMassMatrix,
+                ),
+                fp,
+                indent=4,
+        )
+
+    return
+
+def _handPickedBm(nloptInst, potential, chargedMassMatrix, neutralMassMatrix):
+    bmdictList = []
+    bmInputList = [
+        ##Strong
+        [99.36450394464288, 42.80075856374667, 96.06869518984769, 5.2678728361278155, 0.5086042817226999, 2.5522714805947393, 1],
+        ##Weak
+        [67, 4.0, 50.0, 1.0, 0.0, 2.0 * np.pi / 3, 1],
+        ##2 step complex
+        [68.43630541987292,  79.0888560988479, 45.64676671775482, 27.279729519388148, 0.5364316612973308, 3.082183112574816, 1],
+        ##2 step real
+        [69.13440475054537, 62.67361616764857, 89.67096039014373, 19.87132366433007, 0.49693161396025076, 1.893580908713017, 1],
+        [70, 12.0, 50.0, 1.0, 0.0, 2.0 * np.pi / 3, 1],
+        [75, 55.0, 50.0, 1.0, 0.0, 2.0 * np.pi / 3, 1],
+        [74, 55.0, 50.0, 15.0, 0.0, 2.0 * np.pi / 3, 1],
+        [90, 5.0, 1.0, 1.0, 0.0, 2.0 * np.pi / 3, 1],
+        [90, 55.0, 1.0, 1.0, 0.0, 2.0 * np.pi / 3, 1],
+        [90, 55.0, 1.0, 22.0, 0.0, 2.0 * np.pi / 3, 1],
     ]
 
-    ## I don't like having the function wrapped here,
-    ## should be defined once at a higher level, not every func call but params
-    ## makes that non-trivial
-    def potentialWrapped(fields, _):
-        return potential(fields, params)
+    for bmInput in bmInputList:
+        bmParams = _lagranianParamGen(*bmInput, len(bmdictList))
+        if bmParams:
+            ### IMPORTANT ###
+            ## copy is needed otherwise the background fields enter the 4D beta function
+            ## and lead to small numerical errors (~1e-3%) in the couplings
+            params = copy.copy(bmParams["lagranianParameters"])
+            if checkPhysical(
+                params,
+                nloptInst,
+                potential,
+                chargedMassMatrix,
+                neutralMassMatrix,
+            ):
+                bmdictList.append(bmParams)
 
-    minLocation, minValue = nloptInst.nloptGlobal(potentialWrapped, minimumInitialGuesses[0])
+    return bmdictList
 
-    for guess in minimumInitialGuesses:
-        minLocationTemp, minValueTemp = nloptInst.nloptLocal(potentialWrapped, guess)
+def _randomBmParam(
+    randomNum, nloptInst, potential, chargedMassMatrix, neutralMassMatrix
+):
+    bmdictList = []
+    ## TODO put in some upper limit for this while loop
+    darkHieracy = 1
+    ## Run in parallel?
+    while len(bmdictList) < randomNum:
+        mS1 = np.random.uniform(63, 100)
+        delta12 = np.random.uniform(5, 100)
+        delta1c = np.random.uniform(5, 100)
+        deltac = np.random.uniform(5, 100)
+        ghDM = np.random.uniform(0, 1)
+        thetaCPV = np.random.uniform(np.pi / 2, 3 * np.pi / 2)
 
-        if minValueTemp < minValue:
-            minLocation, minValue = minLocationTemp, minValueTemp
+        bmParams = _lagranianParamGen(
+            mS1, delta12, delta1c, deltac, ghDM, thetaCPV, darkHieracy, len(bmdictList)
+        )
 
-    return np.all(np.isclose(minLocation, [0, 0, higgsVEV], atol=1))
+        if bmParams:
+            if checkPhysical(
+                bmParams["lagranianParameters"],
+                nloptInst,
+                potential,
+                chargedMassMatrix,
+                neutralMassMatrix,
+            ):
+                bmdictList.append(bmParams)
+
+    return bmdictList
+
+def _strongSubSet(prevResultDir):
+    bmdictList = []
+
+    for fileName in glob(join(prevResultDir, "*.json")):
+        with open(fileName, "r") as fp:
+            resultDic = json.load(fp)
+
+        if resultDic["strong"] > 0.6:
+            bmdictList.append(
+                _lagranianParamGen(
+                    resultDic["bmInput"]["mS1"],
+                    resultDic["bmInput"]["delta12"],
+                    resultDic["bmInput"]["delta1c"],
+                    resultDic["bmInput"]["deltac"],
+                    resultDic["bmInput"]["ghDM"],
+                    resultDic["bmInput"]["thetaCPV"],
+                    resultDic["bmInput"]["darkHieracy"],
+                    resultDic["bmNumber"],
+                )
+            )
+
+    return bmdictList
 
 
 def _lagranianParamGen(
@@ -202,163 +285,72 @@ def checkPhysical(params, nloptInst, potential, chargedMassMatrix, neutralMassMa
 
     return True
 
-
-def _randomBmParam(
-    randomNum, nloptInst, potential, chargedMassMatrix, neutralMassMatrix
-):
-    bmdictList = []
-    ## TODO put in some upper limit for this while loop
-    darkHieracy = 1
-    ## Run in parallel?
-    while len(bmdictList) < randomNum:
-        mS1 = np.random.uniform(63, 100)
-        delta12 = np.random.uniform(5, 100)
-        delta1c = np.random.uniform(5, 100)
-        deltac = np.random.uniform(5, 100)
-        ghDM = np.random.uniform(0, 1)
-        thetaCPV = np.random.uniform(np.pi / 2, 3 * np.pi / 2)
-
-        bmParams = _lagranianParamGen(
-            mS1, delta12, delta1c, deltac, ghDM, thetaCPV, darkHieracy, len(bmdictList)
-        )
-
-        if bmParams:
-            if checkPhysical(
-                bmParams["lagranianParameters"],
-                nloptInst,
-                potential,
-                chargedMassMatrix,
-                neutralMassMatrix,
-            ):
-                bmdictList.append(bmParams)
-
-    return bmdictList
-
-
-def _handPickedBm(nloptInst, potential, chargedMassMatrix, neutralMassMatrix):
-    bmdictList = []
-    bmInputList = [
-        ##Strong
-        [99.36450394464288, 42.80075856374667, 96.06869518984769, 5.2678728361278155, 0.5086042817226999, 2.5522714805947393, 1],
-        ##Weak
-        [67, 4.0, 50.0, 1.0, 0.0, 2.0 * np.pi / 3, 1],
-        ##2 step complex
-        [68.43630541987292,  79.0888560988479, 45.64676671775482, 27.279729519388148, 0.5364316612973308, 3.082183112574816, 1],
-        ##2 step real
-        [69.13440475054537, 62.67361616764857, 89.67096039014373, 19.87132366433007, 0.49693161396025076, 1.893580908713017, 1],
-        [70, 12.0, 50.0, 1.0, 0.0, 2.0 * np.pi / 3, 1],
-        [75, 55.0, 50.0, 1.0, 0.0, 2.0 * np.pi / 3, 1],
-        [74, 55.0, 50.0, 15.0, 0.0, 2.0 * np.pi / 3, 1],
-        [90, 5.0, 1.0, 1.0, 0.0, 2.0 * np.pi / 3, 1],
-        [90, 55.0, 1.0, 1.0, 0.0, 2.0 * np.pi / 3, 1],
-        [90, 55.0, 1.0, 22.0, 0.0, 2.0 * np.pi / 3, 1],
+def bPhysicalMinimum(nloptInst, potential, params):
+    ## Move these to user args or somewhere out of the way
+    minimumInitialGuesses = [
+        [0, 0, 0],
+        [0, 0, 246],
+        [100, 100, 100],
+        [-100, 100, 100],
+        [50, 50, 50],
+        [299, 299, 299],
+        [-299, 299, 299],
     ]
 
-    for bmInput in bmInputList:
-        bmParams = _lagranianParamGen(*bmInput, len(bmdictList))
-        if bmParams:
-            ### IMPORTANT ###
-            ## copy is needed otherwise the background fields enter the 4D beta function
-            ## and lead to small numerical errors (~1e-3%) in the couplings
-            params = copy.copy(bmParams["lagranianParameters"])
-            if checkPhysical(
-                params,
-                nloptInst,
-                potential,
-                chargedMassMatrix,
-                neutralMassMatrix,
-            ):
-                bmdictList.append(bmParams)
+    ## I don't like having the function wrapped here,
+    ## should be defined once at a higher level, not every func call but params
+    ## makes that non-trivial
+    def potentialWrapped(fields, _):
+        return potential(fields, params)
 
-    return bmdictList
+    minLocation, minValue = nloptInst.nloptGlobal(potentialWrapped, minimumInitialGuesses[0])
 
+    for guess in minimumInitialGuesses:
+        minLocationTemp, minValueTemp = nloptInst.nloptLocal(potentialWrapped, guess)
 
-def _strongSubSet(prevResultDir):
-    bmdictList = []
+        if minValueTemp < minValue:
+            minLocation, minValue = minLocationTemp, minValueTemp
 
-    for fileName in glob(join(prevResultDir, "*.json")):
-        with open(fileName, "r") as fp:
-            resultDic = json.load(fp)
+    return np.all(np.isclose(minLocation, [0, 0, higgsVEV], atol=1))
 
-        if resultDic["strong"] > 0.6:
-            bmdictList.append(
-                _lagranianParamGen(
-                    resultDic["bmInput"]["mS1"],
-                    resultDic["bmInput"]["delta12"],
-                    resultDic["bmInput"]["delta1c"],
-                    resultDic["bmInput"]["deltac"],
-                    resultDic["bmInput"]["ghDM"],
-                    resultDic["bmInput"]["thetaCPV"],
-                    resultDic["bmInput"]["darkHieracy"],
-                    resultDic["bmNumber"],
-                )
-            )
+def bIsBounded(params):
+    ## Taking equations 26-31 from the draft that ensure the potential is bounded from below.
+    if not params["lamda11"] > 0:
+        return False
+    if not params["lamda22"] > 0:
+        return False
+    if not params["lamda33"] > 0:
+        return False
 
-    return bmdictList
-
-
-def generateBenchmarks(args):
-    (output_file := Path(args.benchmarkFilePath)).parent.mkdir(exist_ok=True, parents=True)
-
-    ## Need to take these from args
-    nloptInst = cNlopt(
-        config={
-            "nbrVars": 3,
-            "absGlobalTol": 0.5,
-            "relGlobalTol": 0.5,
-            "absLocalTol": 0.5,
-            "relLocalTol": 0.5,
-            "varLowerBounds": [-300, 0, 0],
-            "varUpperBounds": [300, 300, 300],
-        }
+    lamdax = params["lamda12"] + min(
+        0, params["lamda12p"] - 2 * m.sqrt(params["lamda1Re"] ** 2 + params["lamda1Im"] ** 2)
     )
-    with open(args.pythonisedExpressionsFilePath, "r") as fp:
-        parsedExpressions = json.load(fp)
-    ## Take the pythonised tree level potential we've generated
-    treeLevel = ParsedExpression(parsedExpressions["veff"]["expressions"], None)
-    chargedMassMatrix = ParsedExpression(
-        parsedExpressions["scalarMassMatrices"]["expressions"][0], None
+    lamday = params["lamda31"] + min(
+        0, params["lamda31p"] - 2 * m.sqrt(params["lamda3Re"] ** 2 + params["lamda3Im"] ** 2)
     )
-    neutralMassMatrix = ParsedExpression(
-        parsedExpressions["scalarMassMatrices"]["expressions"][1], None
+    lamdaz = params["lamda23"] + min(
+        0, params["lamda23p"] - 2 * m.sqrt(params["lamda2Re"] ** 2 + params["lamda2Im"] ** 2)
     )
-
-    ## Feels weird to have nested function but not sure how else to go until can
-    ## use arrays with nlopt
-    def potential(fields, params):
-        params["v1"] = fields[0]
-        params["v2"] = fields[1]
-        params["v3"] = fields[2]
-        return treeLevel.evaluate(params)
-
-    if args.benchmarkType == "randomSSS":
-        with open(output_file, "w") as fp:
-            json.dump(_strongSubSet(args.prevResultDir), fp, indent=4)
-        return
-
-    elif args.benchmarkType == "handPicked":
-        with open(output_file, "w") as fp:
-            json.dump(
-                _handPickedBm(nloptInst, potential, chargedMassMatrix, neutralMassMatrix),
-                fp,
-                indent=4,
-            )
-
-    elif args.benchmarkType == "random":
-        with open(output_file, "w") as fp: 
-            json.dump(
-                _randomBmParam(
-                    args.randomNum,
-                    nloptInst,
-                    potential,
-                    chargedMassMatrix,
-                    neutralMassMatrix,
-                ),
-                fp,
-                indent=4,
-        )
-
-    return
+    if not lamdax > -2 * m.sqrt(params["lamda11"] * params["lamda22"]):
+        return False
+    if not lamday > -2 * m.sqrt(params["lamda11"] * params["lamda33"]):
+        return False
+    if not lamdaz > -2 * m.sqrt(params["lamda22"] * params["lamda33"]):
+        return False
+    if not (
+        m.sqrt(params["lamda33"]) * lamdax
+        + m.sqrt(params["lamda11"]) * lamdaz
+        + m.sqrt(params["lamda22"]) * lamday
+        >= 0
+        or params["lamda33"] * lamdax**2
+        + params["lamda11"] * lamdaz**2
+        + params["lamda22"] * lamday**2
+        - params["lamda11"] * params["lamda22"] * params["lamda33"]
+        - 2 * lamdax * lamday * lamdaz
+        < 0
+    ):
+        return False
+    return True
 
 
 from unittest import TestCase
