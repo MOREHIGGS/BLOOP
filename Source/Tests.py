668067b9e6c40b3534346b2b619ceaeb1fe772a7
Jasmine Thomson-Cooke <jasmine@stp.dias.ie>
2026-03-04 16:55:59 +0000

    WIP: halfway to moving integrations tests to use abs path

--- Source/Tests.py
+++ Source/Tests.py
@@ -2,39 +2,34 @@ import pytest
 import sys
 import subprocess
 import json
-import glob
+from pathlib import Path
 import os
 import difflib
 import time
 import shutil
 
 def runTests():
-    sourceDirectory = os.path.dirname(os.path.realpath(__file__))
-    integrationTestsDirectory = f"{sourceDirectory}/../Share/IntegrationTests"
+    sourceDirectory = Path(__file__).resolve().parent
+    integrationTestsDirectory = sourceDirectory/"../Share/IntegrationTests"
     unitResult = pytest.main([f"{sourceDirectory}/PyTestUnitTests.py"])
     if not unitResult == 0:
         print("Unit tests failed. Skipping integration tests.")
         return
-
+    
     for idx, loopOrder in enumerate(["NLO", "NNLO"]):
         print(f"Running {loopOrder} integration test:")
-        for file in glob.glob("integrationTestDirectory/{loopOrder}/OutputResult/*"):
+        loopDir = integrationTestsDirectory/f'{loopOrder}/'
+        for file in loopDir.glob('OutputResult/*'):
+            print(file)
             os.remove(file)
-
-        for file in glob.glob(f"{sourceDirectory}/../Build/CythonModules/*"):
-            if os.path.isdir(file):
-                shutil.rmtree(file)
-            else:
-                os.remove(file)
-        
         integrationTest = subprocess.run([
             sys.executable,
             f'{sourceDirectory}/RunStages.py',
             '--loopOrder', f'{idx +1}', 
             '--lastBenchmark', '3',
             '--bSave',
-            '--resultsDirectory', f'{integrationTestsDirectory}/{loopOrder}/OutputResult/',
-            '--benchmarkFile', f'{integrationTestsDirectory}/Benchmarks',
+            '--resultsDirectory', f'../Share/IntegrationTests/{loopOrder}/OutputResult',
+            '--benchmarkFile', "../../Share/IntegrationTests/benchmarks.json",
             '--TRangeStart', '90', 
             '--TRangeStepSize', f'1',
             '--TRangeEnd', f'200',
@@ -44,22 +39,22 @@ def runTests():
             capture_output=True,
             text=True,
             )
-
+        
         if not integrationTest.returncode == 0:
             print(f"Error output: {integrationTest.stderr}")
             print(f"{loopOrder} integration test failed, exiting tests.")
             exit() 
         
-        with open(f"{integrationTestsDirectory}/{loopOrder}/OutputResult/ScanResults.json", "r") as fp:
+        with open(loopDir/"OutputResult/ScanResults.json", "r") as fp:
             scanResults = json.load(fp)
-        with open(f"{integrationTestsDirectory}/{loopOrder}/ReferenceResult/ScanResults.json", "r") as fp:
+        with open(loopDir/"ReferenceResult/ScanResults.json", "r") as fp:
             scanResultsRef = json.load(fp)
 
 
         if scanResults ==  pytest.approx(scanResultsRef, rel=0.):
             print(f"Summary of results at {loopOrder} is exactly what we expect")
             continue
-        
+        exit()
         print(f"Summary of results at {loopOrder} is not exactly what we expect")
         with open(f"{loopOrder}Diff.txt", "w") as fp:
             fp.write("Summary diff:")
