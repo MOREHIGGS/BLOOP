8ad465b46995f71837d020011c860ad7a7ff9364
Jasmine Thomson-Cooke <jasmine@stp.dias.ie>
2026-04-01 15:51:15 +0100

    Fixed profile script

--- Share/ProfileCode.sh
+++ Share/ProfileCode.sh
@@ -15,11 +15,11 @@ usage() {
 
 while [[ $# -gt 0 ]]; do
     case $1 in
-        --loopOrder)
+        -l|--loopOrder)
             LOOPORDER="$2"
             shift 2
             ;;
-        --outputFile)
+        -o|--outputFile)
             OUTPUTFILE="$2"
             shift 2
             ;;
@@ -33,10 +33,10 @@ while [[ $# -gt 0 ]]; do
     esac
 done
 
-echo "Generating 20 random benchmarks and compiling modules at loop order $LOOPORDER"
-bloop --lastStage generateBenchmarks --profile --benchmarkType random --maxNumBenchmarks 20 --loopOrder $LOOPORDER || exit -1
+echo "Generating 50 random benchmarks and compiling modules at loop order $LOOPORDER"
+bloop --lastBenchmark 0 --profile --benchmarkType random --maxNumBenchmarks 50 --loopOrder $LOOPORDER --configFilePath $SCRIPT_DIR/../Run/Z2_3HDMConfigFile.json    || exit -1
 echo "Profiling code"
-OMP_NUM_THREADS=1 python3 -m cProfile -o $OUTPUTFILE.pstats $SCRIPT_DIR/../Source/RunStages.py --firstStage doMinimization --lastStage doMinimization || exit -1
+OMP_NUM_THREADS=1 python3 -m cProfile -o $OUTPUTFILE.pstats $SCRIPT_DIR/../Source/RunStages.py --profile --loopOrder $LOOPORDER --benchmarkType load --configFilePath $SCRIPT_DIR/../Run/Z2_3HDMConfigFile.json || exit -1
 gprof2dot --colour-nodes-by-selftime -f pstats $OUTPUTFILE.pstats | \dot -Tsvg -o $OUTPUTFILE.svg || exit -1
 echo "Profile success, results stored in $OUTPUTFILE.svg"
 rm $OUTPUTFILE.pstats || exit -1
