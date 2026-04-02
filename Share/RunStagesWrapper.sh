e0316a2c801ef5b515a8dd545c976de4d48aec54
Jasmine Thomson-Cooke <jasmine@stp.dias.ie>
2026-04-02 10:47:52 +0100

    Have the wrapper just be used for the container

--- Share/RunStagesWrapper.sh
+++ Share/RunStagesWrapper.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
+
+## Wrapper script to run BLOOP in the container by just doing 
+## bloop
+## This also sets OMP_NUM_THREADS to 1 by default to avoid over loading the cpu
+## and allows for auto complete on cmd line arg 
+
 # PYTHON_ARGCOMPLETE_OK
 
-if [ -n "$SHARE_DIR" ]; then
-    ## This is an env variable set in the docker to say we are in container
-    ## as this script is copied to a different location so below doesn't work
-    SHARE_DIR="$SHARE_DIR"
-else
-    SHARE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-fi
-
-## Sets OMP_THREADS to 1 unless user sets it manually with env variabable
-## "$@" needed for autocomplete on cmd line args
-OMP_NUM_THREADS="${OMP_NUM_THREADS:-1}" python3 $SHARE_DIR/../Source/RunStages.py "$@"
+OMP_NUM_THREADS="${OMP_NUM_THREADS:-1}" python3 /Bloop/Source/RunStages.py "$@"
