4d61bcf69341e8cf55a7fe573ea825d46086da33
Jasmine Thomson-Cooke <jasmine@stp.dias.ie>
2026-08-20 11:45:40 +0100

    Plotting code factored, and less magic indices

--- Source/summarise_results.py
+++ Source/summarise_results.py
@@ -7,7 +7,6 @@ from pathlib import Path
 from textwrap import dedent
 
 import numpy as np
-from matplotlib import pylab as plt
 
 
 def summariseResults(args):
@@ -77,8 +76,8 @@ def summariseResults(args):
         
         ## Heat map of first benchmark input vs rest of inputs, and benchmark inputs vs Tc with strength colour bar
         ## first input vs Tc needs to be outside for loop or you'd get first input vs first input plot
-
-        saveHeatMap(
+       
+       saveHeatMap(
             bmInputList[0], 
             TcList, 
             strengthList, 
