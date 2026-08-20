048574b1a9a03f6309fc04ad76ac187d1e37861d
Jasmine Thomson-Cooke <jasmine@stp.dias.ie>
2026-08-20 11:45:40 +0100

    Plotting code factored, and less magic indices

--- Source/summarise_results.py
+++ Source/summarise_results.py
@@ -1,6 +1,9 @@
 import sys
 from collections import defaultdict
 from json import load
+from matplotlib import pylab as plt
+plt.rcParams.update({"font.size": 14})
+import numpy as np
 from pathlib import Path
 from textwrap import dedent
 
@@ -40,10 +43,9 @@ def summariseResults(args):
                 EFTBreak = subResult["EFTBreak"]
                 if EFTBreak:
                     EFTBreakDict[EFTBreak] +=1
+                    if not args.includeEFTBreak:
+                        continue
 
-                if EFTBreak and not args.includeEFTBreak:
-                    continue
-                
                 if subResult["strength"] > strength:
                     strength = subResult["strength"]
                     Tc = subResult["Tc"]
@@ -59,39 +61,61 @@ def summariseResults(args):
     
     ## Sort bmInputs by order of strength, 
     ## this is so the colour of the scatter plot is set by the strong PT at that point
-    ## transpose taken so each row is just on variable type (faster to plot)
     dataSorted =  np.transpose(np.asarray(sorted(zip(strengthList, bmNumberList, TcList, *np.transpose(bmInputList))))) 
+    strengthSorted, bmNumberSorted, TcSorted = dataSorted[:3]
+    bmInputsSorted = dataSorted[3:]
     
     if len(dataSorted) > 0:
         with open(resultsDir/"Summary.txt", "w") as fp:
             fp.writelines(dedent(f"""\
                 Summary of the results: 
-                The total number of benchmarks is: {len(data)}, {len(dataSorted[0])} of which are strong 
+                The total number of benchmarks is: {len(data)}, {len(strengthSorted)} of which are strong 
                 Of the strong phase transitions {multiStepCount} are mutli step
-                The strongest BM is {int(dataSorted[1][-1])} with strength {dataSorted[0][-1]} 
-                Tc min/max is: {min(dataSorted[2])}, {max(dataSorted[2])} 
+                The strongest BM is {int(bmNumberSorted[-1])} with strength {strengthSorted[-1]} 
+                Tc min/max is: {min(TcSorted)}, {max(TcSorted)} 
                 Failure summary: {failDict.items()} 
                 EFT break down summary: {EFTBreakDict.items()} 
                 """))
         
         axisLabels = list(result["bmInput"].keys())
 
-        # Makes plots of first bm Input vs rest of bm inputs
-        for inputIdx, data in enumerate(dataSorted[4:]):
-            plt.scatter(dataSorted[3], data, s=4.2**2, c=dataSorted[0], marker="o")
-            plt.xlabel(axisLabels[0], labelpad=5, fontsize=12)
-            ## +1 needed to skip zeroth element
-            plt.ylabel(axisLabels[inputIdx+1], labelpad=5, fontsize=12)
-            plt.colorbar(label="strength")
-            plt.savefig(resultsDir/f"{axisLabels[inputIdx+1]}")
-            plt.close()
         
-        # Makes plots of bm inputs vs Tc
-        for inputIdx, data in enumerate(dataSorted[3:]):
-            plt.scatter(data, dataSorted[2], s=4.2**2, c=dataSorted[0], marker="o")
-            plt.xlabel(axisLabels[inputIdx], labelpad=5, fontsize=12)
-            plt.ylabel("$T_c$ (GeV)", labelpad=5, fontsize=12)
-            plt.colorbar(label="strength")
-            plt.savefig(resultsDir/f"Tc{axisLabels[inputIdx]}")
-            plt.close()
+        ## Heat map of first benchmark input vs rest of inputs, and benchmark inputs vs Tc with strength colour bar
+        ## first input vs Tc needs to be outside for loop or you'd get first input vs first input plot
+
+        saveHeatMap(
+            bmInputsSorted[0], 
+            TcSorted, 
+            strengthSorted, 
+            axisLabels[0], 
+            "$T_c$", 
+            resultsDir/f"{axisLabels[0]}"
+        )
+        
+        for idx, bmInput in enumerate(bmInputsSorted[1:], 1):
+            saveHeatMap(
+                bmInputsSorted[0], 
+                bmInput, 
+                strengthSorted, 
+                axisLabels[0], 
+                axisLabels[idx], 
+                resultsDir/f"{axisLabels[idx]}"
+            )
+        
+            saveHeatMap(
+                bmInput, 
+                TcSorted, 
+                strengthSorted, 
+                axisLabels[idx], 
+                "$T_c$", 
+                resultsDir/f"{axisLabels[idx]}-Tc"
+           )
+
+def saveHeatMap(x, y, c, xLabel, yLabel, fileName, norm=None):
+    plt.scatter(x, y, c=c, s=4.2**2, marker ="o", norm = norm)
+    plt.xlabel(xLabel, labelpad=5)
+    plt.ylabel(yLabel, labelpad=5)
+    plt.colorbar(label="strength")
+    plt.savefig(fileName)
+    plt.close()
 
