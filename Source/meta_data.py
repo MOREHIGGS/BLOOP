fc09257ba3973787522a4f7ba9b559ef800ea92f
Jasmine Thomson-Cooke <jasmine@stp.dias.ie>
2026-08-27 10:20:45 +0100

    Remove ruff error

--- Source/meta_data.py
+++ Source/meta_data.py
@@ -44,7 +44,7 @@ def getGitInfo(debug):
             command, 
             capture_output=True, 
             text=True,
-            ).stdout.splitlines()
+            check=False).stdout.splitlines()
         for key, command in {
             "git tag": ["git", "describe", "--tags", "--always"],
             "git branch": ["git", "rev-parse", "--abbrev-ref", "HEAD"],
