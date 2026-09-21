80d4af84ca0321651b764d6be093ddacf1054a42
Jasmine Thomson-Cooke <jasmine@stp.dias.ie>
2026-09-21 15:48:46 +0100

    Moved nlopt class to utility

--- Source/z2_3hdm_bm_generator.py
+++ Source/z2_3hdm_bm_generator.py
@@ -12,8 +12,7 @@ import pdg
 import pytest
 from scipy.constants import physical_constants as constants
 
-from dataclasses import InitVar, dataclass
-import nlopt
+from utility import cNlopt
 
 ## Connecting is expensive so just do it once in global space to avoid passing it around
 api = pdg.connect()
@@ -508,39 +507,4 @@ class BmGeneratorUnitTests(TestCase):
             )
         except AssertionError:
             pytest.xfail("Fermi constant from Scipy isn't what we expect")
-
-@dataclass(frozen=True)
-class cNlopt:
-    nbrVars: int = 0
-    varLowerBounds: tuple[float] = (0,)
-    varUpperBounds: tuple[float] = (0,)
-    absLocalTol: float = 0
-    relLocalTol: float = 0
-    absGlobalTol: float = 0
-    relGlobalTol: float = 0
-    config: InitVar[dict] = None
-
-    def __post_init__(self, config: dict):
-        if config:
-            self.__init__(**config)
-
-    def nloptGlobal(self, func: callable, initialGuess: list[float]):
-        opt = nlopt.opt(nlopt.GN_DIRECT_NOSCAL, self.nbrVars)
-        opt.set_min_objective(func)
-        opt.set_lower_bounds(self.varLowerBounds)
-        opt.set_upper_bounds(self.varUpperBounds)
-        opt.set_xtol_abs(self.absGlobalTol)
-        opt.set_xtol_rel(self.relGlobalTol)
-        opt.set_exceptions_enabled(False)
-        return self.nloptLocal(func, opt.optimize(initialGuess))
-
-    def nloptLocal(self, func: callable, initialGuess: list[float]):
-        opt = nlopt.opt(nlopt.LN_BOBYQA, self.nbrVars)
-        opt.set_min_objective(func)
-        opt.set_lower_bounds(self.varLowerBounds)
-        opt.set_upper_bounds(self.varUpperBounds)
-        opt.set_xtol_abs(self.absLocalTol)
-        opt.set_xtol_rel(self.relLocalTol)
-        opt.set_exceptions_enabled(False)
-        return opt.optimize(initialGuess), opt.last_optimum_value(), opt.last_optimize_result()
 
